4a3f90f9d54a54367bfdb73a0c27b528305fe83e
Cameron White <cameronwhite16@gmail.com>
2020-04-26 13:42:22 -0600

    Adding form for getting tomatometer details

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,13 @@
 <script>
+	import CompareTomatoMeter from './components/CompareTomatoMeter.svelte';
 	export let name;
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+
+	<CompareTomatoMeter />
 </main>
 
 <style>
---
+++ src/components/CompareTomatoMeter.svelte
@@ -0,0 +1,15 @@
+<script>
+    import axios from 'axios';
+    let name = "";
+
+    function handleSubmit(e) {
+        axios.get(`/api/get-total-pages/type/m/name/${name}`)
+    }
+</script>
+
+<form on:submit|preventDefault={handleSubmit}>
+    <label>Movie Name</label>
+    <input type=text on:change={(e) => name = e.target.value}/>
+
+    <input type="submit" value="Check honesty" />
+</form>
