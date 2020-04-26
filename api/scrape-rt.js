38e971decb340d8ea7c182dfa2aca51a1f5bb2e4
Cameron White <cameronwhite16@gmail.com>
2020-04-25 20:14:37 -0600

    Properly parsing integers. I swear I've done this before

--- api/scrape-rt.js
+++ api/scrape-rt.js
@@ -2,8 +2,9 @@ const apiErrorHandler = require('../lib/util/api-error-handler');
 
 module.exports = ({query: {name, type, page}}, res) => {
     let resultPromise;
+    page = parseInt(page);
     
-    if (typeof page === 'number') {
+    if (typeof page === 'number' && !Number.isNaN(page)) {
         resultPromise = require('../lib/rt/get-page-review-results')({name, type, page});
     } else {
         resultPromise = require('../lib/rt/scrape-reviews')({name, type});
